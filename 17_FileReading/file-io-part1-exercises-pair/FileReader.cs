5bb03bde60fb2b7c351ffe8b6bdfd7604cc28737
Andrew Miller <andrewmiller.3d@gmail.com>
2020-06-03 11:27:24 -0400

    Pairs project showing good results

--- 17_FileReading/file-io-part1-exercises-pair/FileReader.cs
+++ 17_FileReading/file-io-part1-exercises-pair/FileReader.cs
@@ -9,7 +9,7 @@ namespace file_io_part1_exercises_pair
     class FileReader
     {
         
-        public static string TextReader()
+        public static void TextReader()
         {
             int sentenceCount = 0;
             int wordCount;
@@ -41,10 +41,10 @@ namespace file_io_part1_exercises_pair
             wordCount = allWords.Count;
             string finalWordCount = $"Word Count: {wordCount}";
             string finalSentenceCount = $"Sentence Count: {sentenceCount}";
-            return finalWordCount;
-            return finalSentenceCount;
-            //Console.WriteLine(finalWordCount);
-            //Console.WriteLine(finalSentenceCount);
+            //return finalWordCount;
+            //return finalSentenceCount;
+            Console.WriteLine(finalWordCount);
+            Console.WriteLine(finalSentenceCount);
         }
     }
 }
